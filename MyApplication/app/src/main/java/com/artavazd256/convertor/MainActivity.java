4919efb71f3716b3643d1d1c800ed30f44cc0fdf
Artavazd256 <Artavazd256@users.noreply.github.com>
2015-07-13 20:57:06 +0400

    Added converter  application in github

---
+++ MyApplication/app/src/main/java/com/artavazd256/convertor/MainActivity.java
@@ -0,0 +1,16 @@
+package com.artavazd256.convertor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by artavazd on 7/13/15.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
